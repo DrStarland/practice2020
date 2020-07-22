e07f04a73cd8e7c3c5d70214cc3a853e78cef487
DrStarland <66081492+DrStarland@users.noreply.github.com>
2020-07-22 23:18:29 +0300

    Add files via upload

--- main-server.cpp
+++ main-server.cpp
@@ -9,77 +9,18 @@ using std::cin; using std::cout; using std::endl;
 
 int main()
 {
-//    if (!socket_test()) {
-//        std::cout << "eh";
-//    }
-
     UDPBasedTransmittion system(27000);
 
     Address destination(127, 0, 0, 1, 26000);
-    //http://176.195.28.243
     //Address destination(176, 195, 28, 243, 26000);
 
-    char name[] = "test.png";
+    char name[30];
     cout << "activity test " << system.isActive() << endl;
-    {
-        FileDescriptor file(name, "rb");
-        cout << "test length " << file.countPackets(MAX_PACKET_LEN) << std::endl;
-    }
+    сout << "Input name of file: \n";
+    cin >> name;
 
-
-    cout << system.sendFile(destination, name);
+    cout << "Result of sending: "<< system.sendFile(destination, name);
 
     cout << "\nall";
     return 0;
 }
-
-//system.send(destination, name, 9);
-
-//for (int i = 0; i < 10; i++) {
-//    Address sender;
-//    char buffer[25];
-//    if (system.receive( sender, buffer, 25 ) > 0) {
-//        cout << buffer << endl;
-//    }
-//    else
-//        break;
-//}
-
-bool socket_test() {
-    SocketController test;
-
-    unsigned short port = 30000;
-    Socket socket;
-    socket.open(port);
-
-    unsigned char a = 127, b = 0, c = 0, d = 1;
-    Address target(a, b, c, d, port);
-
-    const char packet_data[] = "hello world!";
-    int packet_size = 14;
-
-    int result = socket.send(target, packet_data, packet_size);
-    if ( !result ) {
-        printf( "failed to send packet" );
-        return false;
-    }
-    else
-        cout << " all okey " << result << endl;
-
-    for (int i = 0; i < 10; i++) {
-        Address sender;
-        char buffer[256];
-        if (socket.receive( sender, buffer, 255 ) > 0) {
-            char buf2[14];
-            strncpy(buf2, buffer, 14);
-            cout << buf2 << std::endl;
-        }
-        else
-            break;
-    }
-
-    socket.close();
-
-    return true;
-}
-
--- udpbasedtransmittion.cpp
+++ udpbasedtransmittion.cpp
@@ -52,7 +52,6 @@ bool UDPBasedTransmittion::sendFile(const Address &destination, const char *file
             break;
         }
 
-        // сделать приём завершающей декларации или запроса
     for (size_t over_i = 0; over_i < 4; ++over_i) {
         FileDeclaration final_order;
         int temp;
@@ -65,11 +64,9 @@ bool UDPBasedTransmittion::sendFile(const Address &destination, const char *file
                     }
                     else {
                         size_t control_sum = *((size_t*) final_order.getComment());
-                        cout << "eto odin? " << control_sum << std::endl;
                         for (size_t i = 0; i < 15000000 && control_sum; i++) {
                             Infopacket buffer;
                             if (receive(sender, &buffer, sizeof(Infopacket))) {
-                                cout << "sum packet " << control_sum << std::endl;
                                 UDPbuffer.push_front(buffer);
                                 --i, --control_sum;
                             }
@@ -79,18 +76,13 @@ bool UDPBasedTransmittion::sendFile(const Address &destination, const char *file
                             UDPbuffer.pop_front();
                             size_t *arr = (size_t*) buf_pack.getData();
                             for (size_t j = 0; j < buf_pack.getDataLength() / sizeof(size_t); ++j) {
-                                if (j % 5 == 0)
-                                    cout << "///\n";
                                 Infopacket buffer;
                                 buffer.reset(file, arr[j], my_ID);
                                 send(destination, &buffer, sizeof(Infopacket));
                             }
                         }
-                        cout << "spravilis\n";
                     }
                 }
-                else
-                    cout << "nu chto-to ushlo\n";
             }
         }
     }
@@ -109,11 +101,8 @@ bool UDPBasedTransmittion::receiveFile(FileDeclaration &order, const char *anoth
     unsigned char stop = 4;
     bool sort_flag = false;
     while (!order.isCompleted() && stop) {
-        cout << "this was working twice, control " << control_sum << std::endl;
         for (size_t i = 0; i < 150000 && control_sum; i++) {
             if (receive(sender, &buf_pack, sizeof(Infopacket))) {
-                if (buf_pack.getNumber() == 228)
-                    cout << "228 incoming!!\n";
                 list_of_packets[buf_pack.getNumber()] = 1;
                 UDPbuffer.push_back(buf_pack);
                 --i, --control_sum;
@@ -125,18 +114,10 @@ bool UDPBasedTransmittion::receiveFile(FileDeclaration &order, const char *anoth
             if (i == 0)
                 ++count_of_lacks;
 
-        cout << "Count of lossed packets: " << count_of_lacks << std::endl;
-
         if (count_of_lacks == 0) {
-            order.complete();
-            cout << "result sending " << send(sender, &order, sizeof(FileDeclaration)) << "\n";
-            break;
+            order.complete(); break;
         }
         else {
-//            order.complete();
-//            cout << "result sending " << send(sender, &order, sizeof(FileDeclaration)) << "\n";
-//            break;
-            cout << "!\n";
             shared_ptr<size_t[]> dop_list(new size_t[count_of_lacks]);
             {
                 size_t j = 0, number = 0;
@@ -146,36 +127,29 @@ bool UDPBasedTransmittion::receiveFile(FileDeclaration &order, const char *anoth
                     ++number;
                 }
             }
-            cout << "!!\n";
+
             size_t amount_in_packet = MAX_PACKET_LEN / sizeof(size_t);
             size_t amount_packets = ceil((double) count_of_lacks / (double) amount_in_packet);
             order.setComment(&amount_packets, sizeof(size_t));
             send(sender, &order, sizeof(FileDeclaration));
-            cout << "!!!\n";
-            cout << "amount_in_packet " << amount_in_packet << std::endl;
-            cout << "amount_packets " << amount_packets << " dop info " << count_of_lacks / amount_in_packet << std::endl;
             Infopacket buf_pack;
             size_t i = 0;
             for (; i < count_of_lacks / amount_in_packet; ++i) {
                 buf_pack.reset(dop_list.get() + i * amount_in_packet, amount_in_packet * sizeof(size_t), i, order.getID());
                 send(sender, &buf_pack, sizeof(Infopacket));
             }
-            cout << "!!!!\n";
-            cout << "i = " << i << std::endl;
             if (count_of_lacks % amount_in_packet != 0) {
                 buf_pack.reset(dop_list.get() + i * amount_in_packet,
                                (count_of_lacks % amount_in_packet) * sizeof(size_t), i, order.getID());
-                cout << (count_of_lacks % amount_in_packet) << ", " << (count_of_lacks % amount_in_packet) * sizeof(size_t) << std::endl;
                 send(sender, &buf_pack, sizeof(Infopacket));
             }
-            // собрать в функцию
-            cout << "!!!!!\n";
             control_sum = count_of_lacks;
             sort_flag = true;
             --stop;
         }
     }
 
+    // написать свою очистку, затрагивающую только этот файл
     if (stop == 0 && !order.isCompleted()) {
         UDPbuffer.clear();
         return false;
@@ -184,12 +158,11 @@ bool UDPBasedTransmittion::receiveFile(FileDeclaration &order, const char *anoth
     if (sort_flag)
         UDPbuffer.sort();
 
+    // сделать запись из буфера в файл в отдельную функцию
     size_t j = 0;
     for (Infopacket& i : UDPbuffer) {
         file.setPacket(i.getData(), i.getDataLength(), j++);
     }
-
-    //сделать запрос недошедших пакетов или отчёт о полном получении - вернуть завершённую декларацию
     return true;
 }
 
